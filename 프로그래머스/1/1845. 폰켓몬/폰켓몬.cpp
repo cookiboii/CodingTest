b937a292e68ed3f7f708b1158fda57e77c3dd959
naseokhoo <nahoo1202@naver.com>
2023-12-07 17:14:42 +0900

    [level 1] Title: 폰켓몬, Time: 0.27 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -10,7 +10,11 @@ int solution(vector<int> nums)
      
     sort(nums.begin(),nums.end(),greater<>());// 내림 차순정렬 
    nums.erase(unique(nums.begin(),nums.end()),nums.end()); // 중복된거 지운다
-     answer=n>nums.size()?nums.size():n;  // 
+    if (n > nums.size()) {
+    answer = nums.size();
+} else {
+    answer = n;
+}  // 
     
     
     return answer;
