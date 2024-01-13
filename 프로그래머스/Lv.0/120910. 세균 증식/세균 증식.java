ac2bf37722c1b2dac9d70a87b50a47272de96b36
naseokhoo <nahoo1202@naver.com>
2024-01-13 11:44:33 +0900

    [level Lv.0] Title: 세균 증식, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,16 @@
+
+
+class Solution {
+ 
+ 
+    
+    public int solution(int n, int t) {
+        int answer = 0;
+    
+      answer = (int) (n * Math.pow(2, t));
+        
+       
+        
+        return answer;
+    }
+}
