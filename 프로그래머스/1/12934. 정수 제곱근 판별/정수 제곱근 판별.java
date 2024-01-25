0fab0cb234c9d9815d844f51f2faaa57b5e528f0
naseokhoo <nahoo1202@naver.com>
2024-01-25 18:48:49 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        long number = (long)Math.sqrt(n);
+      if (number *number ==n){
+          answer = (number+1)*(number+1);
+      }
+        else{
+            answer =-1;
+        }
+        
+        return answer;
+    }
+}
