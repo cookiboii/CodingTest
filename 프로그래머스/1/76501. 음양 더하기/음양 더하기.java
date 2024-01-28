13fba888e858f9ac0863aa9de24997fd12cfbe2f
naseokhoo <nahoo1202@naver.com>
2024-01-28 23:19:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+           int num =0;  
+
+        for (int i =0; i<absolutes.length;i++){
+           if (signs[i]){
+               
+               answer += absolutes[i];
+           } 
+            else{
+                
+                answer -=absolutes[i];
+            }
+            
+        }
+        
+        
+        return answer;
+    }
+}
