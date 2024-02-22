7d4f6e29995ca888ab959d67d5a59d243e4ae903
naseokhoo <nahoo1202@naver.com>
2024-02-23 07:17:35 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,14 +1,16 @@
+
+import java.util.*;
+
 class Solution {
-    public int[] solution(int n) {
-        int[] answer = new int[n / 2 + n % 2];
-        int index = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                answer[index++] = i;
-            }
+    public ArrayList solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++){
+          if(i%2 != 0) {
+              answer.add(i);
+          } 
         }
+
         return answer;
     }
 }
-
 
