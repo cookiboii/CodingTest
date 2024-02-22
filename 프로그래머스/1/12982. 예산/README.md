5fd5386ce333a49a2907b559771bf6f50bd5469f
naseokhoo <nahoo1202@naver.com>
2024-02-23 06:57:11 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.32 ms
+메모리: 72.6 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 23일 06:29:42
+2024년 02월 23일 06:57:09
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
+
+
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-      
-        Arrays.sort(d);
-        
-        for (int i = 0; i < d.length; i++) {
-            if (budget < d[i]) {
-                break;
+        int[] dept = Arrays.copyOf(d, d.length);
+        Arrays.sort(dept);
+        int sum = 0;
+        for (int inx=0; inx<dept.length; inx++) {
+            sum += dept[inx];
+            if (sum<=budget) {
+                answer++;
             }
-            budget -= d[i];
-            answer++;
         }
-        
         return answer;
     }
 }
