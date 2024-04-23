372f56e279ffb6768d90f088fe989d550fb5d450
naseokhoo <nahoo1202@naver.com>
2024-04-23 23:46:12 +0900

    [level 1] Title: 실패율, Time: 0.78 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,36 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        double[] tempArr = new double[N];
-        int arrLength = stages.length;
-        int idx = arrLength;
-        double tempD = 0;
-        int tempI = 0;
-        for (int i = 0; i < arrLength; i++) {
-            int stage = stages[i];
-            if (stage != N + 1)
-                answer[stage - 1] += 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Double> falseMap = new HashMap<>();
+        double totalUser = (double) stages.length;
+        
+        // 각 스테이지에 도달한 플레이어 수 계산
+        for (int i = 0; i < stages.length; i++) {
+            map.put(stages[i], map.getOrDefault(stages[i], 0) + 1);
         }
-        for (int i = 0; i < N; i++) {
-            int personNum = answer[i];
-            tempArr[i] = (double) personNum / idx;
-            idx -= personNum;
-            answer[i] = i + 1;
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j < N - i; j++) {
-                if (tempArr[j - 1] < tempArr[j]) {
-                    tempD = tempArr[j - 1];
-                    tempArr[j - 1] = tempArr[j];
-                    tempArr[j] = tempD;
-
-                    tempI = answer[j - 1];
-                    answer[j - 1] = answer[j];
-                    answer[j] = tempI;
-                }
+        
+        // 스테이지 실패율 계산
+        for (int i = 1; i <= N; i++) {
+            if (map.containsKey(i)) {
+                falseMap.put(i, map.get(i) / totalUser);
+                totalUser -= map.get(i);
+            } else {
+                falseMap.put(i, 0.0);
             }
         }
+        
+        // 실패율 내림차순으로 정렬
+        List<Integer> sortStages = new ArrayList<>(falseMap.keySet());
+        Collections.sort(sortStages, (s1, s2) -> falseMap.get(s2).compareTo(falseMap.get(s1)));
+        
+        // 정렬된 스테이지 번호를 배열에 저장
+        for (int i = 0; i < N; i++) {
+            answer[i] = sortStages.get(i);
+        }
+        
         return answer;
     }
 }
