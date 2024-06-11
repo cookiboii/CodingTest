7e54e973df95fc83a38a3cb3d9d10223d56e3ae3
naseokhoo <nahoo1202@naver.com>
2024-06-11 23:33:44 +0900

    [level 3] Title: 기지국 설치, Time: 0.76 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,20 +1,22 @@
 class Solution {
     public int solution(int N, int[] stations, int W) {
-        int answer = 0;
-        int current = 1;
-        int index = 0;
-        int range = 2 * W + 1;
+        int answer = 0; // 추가로 설치해야 할 기지국의 수
+        int current = 1; // 현재 전파가 도달하지 않는 첫 아파트의 위치
+        int index = 0; // stations 배열의 인덱스
+        int range = 2 * W + 1; // 한 기지국이 커버할 수 있는 아파트의 범위
         
         while (current <= N) {
             if (index < stations.length && stations[index] - W <= current) {
-                current = stations[index] + W + 1;
-                index++;
+                // 현재 기지국이 커버하는 범위 내에 있다면
+                current = stations[index] + W + 1; // 해당 기지국의 커버 범위 끝 다음 아파트로 이동
+                index++; // 다음 기지국으로 이동
             } else {
-                answer++;
-                current += range;
+                // 현재 기지국이 커버하는 범위 밖에 있다면
+                answer++; // 새로운 기지국을 추가
+                current += range; // 새로운 기지국의 커버 범위 끝 다음 아파트로 이동
             }
         }
   
-        return answer;
+        return answer; // 추가로 설치해야 할 기지국의 수 반환
     }
 }
