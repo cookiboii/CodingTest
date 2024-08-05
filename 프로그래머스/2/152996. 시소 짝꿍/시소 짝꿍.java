27f27eeeb874b4c04072cad1c9b91b29e40516ef
naseokhoo <nahoo1202@naver.com>
2024-08-06 03:30:06 +0900

    [level 2] Title: 시소 짝꿍, Time: 131.80 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,32 +1,24 @@
+
+
+import java.util.*;
+
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        int[] count = new int[4001];  // 무게의 범위는 1부터 4000까지이므로 크기를 4001로 설정
+        Arrays.sort(weights);
+        HashMap<Double,Integer> map = new HashMap<>();
 
-        // 주어진 weights 배열에서 각 무게의 빈도를 카운트
-        for (int weight : weights) {
-            count[weight]++;
-        }
-
-        // 중복 없이 weights 배열을 사용하여 계산
-        for (int i = 100; i <= 1000; i++) {
-            for (int j = i; j <= 4000; j++) {
-                // 같은 무게의 경우
-                if (i == j) {
-                    answer += (long) count[i] * (count[i] - 1) / 2;
-                } else {
-                    if (i * 2 == j) {
-                        answer += (long) count[i] * count[j];
-                    }
-                    if (i * 3 == j * 2) {
-                        answer += (long) count[i] * count[j];
-                    }
-                    if (i * 4 == j * 3) {
-                        answer += (long) count[i] * count[j];
-                    }
-                }
+        for(Integer weight:weights){
+            double w = Double.valueOf(weight);
+            if(map.containsKey(w)){
+                answer+=map.get(w);
             }
+            map.put(w,map.getOrDefault(w,0)+1);
+            map.put(w*4/3,map.getOrDefault(w*4/3,0)+1);
+            map.put(w*1.5,map.getOrDefault(w*1.5,0)+1);
+            map.put(w*2,map.getOrDefault(w*2,0)+1);
         }
+
 
         return answer;
     }
