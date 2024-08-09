028ed3d8d1bc3d82707695089db7d07725efe06e
naseokhoo <nahoo1202@naver.com>
2024-08-09 15:11:30 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.08 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int [arr.length];
+        
+       for(int [] query:queries){
+          int s= query[0];
+          int e =query[1];
+            for(int i =s; i<=e;i++){
+             arr[i]++;
+        }
+       }
+
+        
+        return arr;
+    }
+}
