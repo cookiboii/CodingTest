0c20bd544f7d9c9dfd23a18fd4e9cf6d30b0aa93
naseokhoo <nahoo1202@naver.com>
2024-12-22 23:57:06 +0900

    [level 0] Title: 짝수의 합, Time: 0.05 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,19 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-         
-        for(int i=0; i<=n;i++){
-            if(i%2==0){
-                 answer+=i;
-                
-                
-            }
+        int sum =0;
+        for(int i =1; i<=n;i++){
             
+            if(i%2==0){
+                sum +=i;
+            }
+            answer =sum;
         }
-        
-      
-        
-        
         return answer;
     }
 }
