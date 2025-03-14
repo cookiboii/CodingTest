559c357a83f303e61d7e423d580f12f8dc35ffb2
naseokhoo <nahoo1202@naver.com>
2025-03-14 11:25:38 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 19476 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,44 +1,33 @@
+import java.io.*;
+import java.util.*;
 
-
-import java.util.Scanner;
-import java.util.Stack;
-
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
-
-        int n = sc.nextInt();  // 명령어 개수 입력
-        sc.nextLine();  // 개행 문자 제거
-
+        
+        int n = Integer.parseInt(br.readLine()); // 명령어 개수 입력
+        
         for (int i = 0; i < n; i++) {
-            String command = sc.next();  // 명령어 입력
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken(); // 명령어 입력
 
             if (command.equals("push")) {
-                int num = sc.nextInt();
+                int num = Integer.parseInt(st.nextToken());
                 stack.push(num);
-            }
-            else if (command.equals("pop")) {
-                if (stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.pop());
-                }
-            }
-            else if (command.equals("size")) {
-                System.out.println(stack.size());
-            }
-            else if (command.equals("empty")) {
-                System.out.println(stack.isEmpty() ? 1 : 0);
-            }
-            else if (command.equals("top")) {
-                if (stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.peek()); // getLast() 대신 peek() 사용
-                }
+            } else if (command.equals("pop")) {
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+            } else if (command.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (command.equals("empty")) {
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+            } else if (command.equals("top")) {
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
             }
         }
-        sc.close();
+        
+        System.out.print(sb);
+        br.close();
     }
 }
