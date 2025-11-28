f3bef0f2c0e28fad34a5f1ff445bf3ee7b73c651
naseokhoo <nahoo1202@naver.com>
2025-11-28 23:04:26 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,29 +1,28 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // n 입력 받기
-        int n = Integer.parseInt(br.readLine());
-        
-        // DP 테이블 생성 (n은 최대 1000이므로 1001 크기로 잡음)
-        int[] dp = new int[1001];
-        
-        // 초기값 세팅 (Base Case)
-        dp[1] = 1;
-        if (n >= 2) { // n이 1일 수도 있으므로 예외 처리
-            dp[2] = 3;
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     int N = Integer.parseInt(br.readLine());
+
+     int[] dp  = new int [1001];
+
+     dp[1] =1;
+     if ( N >=2){
+         dp[2] =3;
+     }
+
+        for (int i = 3; i <=N ; i++) {
+
+          dp[i] = (dp[i-1]+2*dp[i-2])%10007;
         }
-        
-        // 점화식 적용 (Bottom-Up 방식)
-        for (int i = 3; i <= n; i++) {
-            // 점화식: dp[i] = dp[i-1] + 2 * dp[i-2]
-            // 문제 조건에 따라 10007로 나눈 나머지 저장
-            dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
-        }
-        
-        System.out.println(dp[n]);
+
+      System.out.println(dp[N]);
     }
 }
