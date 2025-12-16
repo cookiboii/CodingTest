ed7e464135d56f82188e93a71f29b6de486c4c1d
naseokhoo <nahoo1202@naver.com>
2025-12-17 01:19:56 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+
+    }
+
+    public static int factorial(int n){
+          if(n==0)
+            return 1;
+        if (n == 1) {
+            return 1;
+        }
+          else{
+              return n * factorial(n-1);
+        }
+
+    }
+}
